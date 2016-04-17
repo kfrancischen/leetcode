498b1602067e01c4aade75ce42baa9b279559c61
kfrancischen <kfrancischen@gmail.com>
2016-04-16 22:55:31 -0700

    add two sum code

---
+++ cPlusPlus/1_two-sum/Solution.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+	public:
+		vector<int> twoSum(vector<int& nums, int target){
+			int length = nums.size();
+			if length < 2
+				return;
+			else
+				for(
+		}
+};
