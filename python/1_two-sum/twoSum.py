929d784ede7ec118e43121b61d00ada3a3c7165e
kfrancischen <kfrancischen@gmail.com>
2016-04-16 22:49:38 -0700

    add two sum code

---
+++ python/1_two-sum/twoSum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+	def twoSum(self, nums, target):
+		lengthOfNums = len(nums)
+		if lengthOfNums < 2:
+			return
+		else:
+			for firstPointer in range(0, lengthOfNums):
+				for secondPointer in range(firstPointer + 1, lengthOfNums):
+					if nums[firstPointer] + nums[secondPointer] == target:
+						return [firstPointer, secondPointer]
+			
+			if firstPointer == lengthOfNums and secondPointer == lengthOfNums:
+				return
+
+mytest = Solution()
+print mytest.twoSum([1,2],3)
+print mytest.twoSum([1,2,3],3)
+print mytest.twoSum([3,2,1],3)
