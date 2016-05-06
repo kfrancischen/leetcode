c97b6101463faefd3746c3ae96b95768277966e8
kfrancischen <kfrancischen@gmail.com>
2016-05-05 22:15:35 -0700

    finished 88

---
+++ python/88_merge-sorted-array/mergeSortedArray.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while k >= 0:
+            if i < 0:
+                nums1[k] = nums2[j]
+                j -= 1
+            elif j < 0:
+                nums1[k] = nums1[i]
+                i -= 1
+            elif nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+mytest = Solution()
+nums1 = [4,6,0,0,0]
+m = 2
+nums2 = [1,2,3]
+n = 3
+mytest.merge(nums1, m, nums2, n)
+print nums1
