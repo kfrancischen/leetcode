87e7f621fc86f8f886862eccf4a7d4c0844aad38
kfrancischen <kfrancischen@gmail.com>
2016-06-02 14:31:34 -0700

    finished 264

---
+++ python/264_ugly-number-II/uglyNumberII.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 0:
+            return 0
+        ugly = [1] * n
+        l2 = l3 = l5 = 0
+        for i in range(1, n):
+            ugly[i] = min(ugly[l2] * 2, ugly[l3] * 3, ugly[l5] * 5)
+            if ugly[i] == ugly[l2] * 2:
+                l2 += 1
+            if ugly[i] == ugly[l3] * 3:
+                l3 += 1
+            if ugly[i] == ugly[l5] * 5:
+                l5 += 1
+        return ugly[-1]
+
+
+mytest = Solution()
+n = 5
+print mytest.nthUglyNumber(n)
