8fa42ad479f063ca98b8c8610d5c532815323754
kfrancischen <kfrancischen@gmail.com>
2016-06-02 14:41:44 -0700

    finished 326

---
+++ python/326_power-of-three/powerOfThree.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and 1162261467 % n == 0
+
+mytest = Solution()
+n = 9
+print mytest.isPowerOfThree(n)
