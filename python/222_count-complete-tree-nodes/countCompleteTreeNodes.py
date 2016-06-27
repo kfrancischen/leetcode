5a2c49e628e236cf604b49b041b211774607e977
francischen <kfrancischen@gmail.com>
2016-06-27 16:15:10 -0700

    finished 222

---
+++ python/222_count-complete-tree-nodes/countCompleteTreeNodes.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def countNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        count = 0
+        while root:
+            l, r = map(self.getHeight, (root.left, root.right))
+            if l == r:
+                count += 2 ** l
+                root = root.right
+            else:
+                count += 2 ** r
+                root = root.left
+
+        return count
+
+
+    def getHeight(self, root):
+        height = 0
+        while root:
+            height += 1
+            root = root.left
+        return height
