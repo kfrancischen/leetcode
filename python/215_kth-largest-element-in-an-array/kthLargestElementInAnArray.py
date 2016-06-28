049767125d0732b1c4513f7bd19550499a9712ef
francischen <kfrancischen@gmail.com>
2016-06-27 20:38:23 -0700

    finished 215

--- python/215_kth-largest-element-in-an-array/kthLargestElementInAnArray.py
+++ python/215_kth-largest-element-in-an-array/kthLargestElementInAnArray.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution(object):
     def findKthLargest(self, nums, k):
         """
@@ -5,3 +6,17 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        result = []
+        for i in range(0,k):
+            heapq.heappush(result, nums[i])
+            i += 1
+        while i < len(nums):
+            if result[0] < nums[i]:
+                heapq.heappushpop(result, nums[i])
+            i += 1
+        return result[0]
+
+mytest = Solution()
+s = [3,2,1,5,6,4]
+k = 2
+print mytest.findKthLargest(s,k)
