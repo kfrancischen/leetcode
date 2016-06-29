ce1a6233de03999ec2e8c5a1b0404294df4c50c8
francischen <kfrancischen@gmail.com>
2016-06-28 20:46:34 -0700

    finished 300, marked

---
+++ python/300_longest-increasing-subsequence/longestIncreasingSubsequence.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i != j:
+                m = (i + j) / 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(i + 1, size)
+        return size
+
+
+mytest = Solution()
+s = [10, 9, 2, 5, 3, 7, 101, 18]
+print mytest.lengthOfLIS(s)
