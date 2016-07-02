f48d1bc34cc9d9f9524c1fdd4192b10bc4ad064a
francischen <kfrancischen@gmail.com>
2016-07-02 10:50:35 -0700

    finished 309, marked

---
+++ python/309_best-time-to-buy-and-sell-stock-with-cooldown/bestTimeToBuyAndSellStockWithCooldown.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        notHold, notHold_cooldown, hold = 0, float('-inf'), float('-inf')
+        for p in prices:
+            hold, notHold, notHold_cooldown = max(hold, notHold - p), max(notHold, notHold_cooldown), hold + p
+        return max(notHold, notHold_cooldown)
