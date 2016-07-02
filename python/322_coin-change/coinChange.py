0f8119c9d53a99c2431bcd0db440c5ef59658eb9
francischen <kfrancischen@gmail.com>
2016-07-01 22:48:15 -0700

    finished 322, marked

--- python/322_coin-change/coinChange.py
+++ python/322_coin-change/coinChange.py
@@ -8,15 +8,16 @@ class Solution(object):
         if amount == 0:
             return 0
         dp = [amount+1]*(1+amount)
-        print dp
+
         dp[0] = 0
         for i in xrange(1,amount+1):
             for coin in coins:
                 if coin<=i:
                     dp[i] = min(dp[i],dp[i-coin]+1)
+        print dp
         return -1 if dp[-1] == amount+1 else dp[-1]
 
 mytest = Solution()
-coins = [186, 419, 83, 408]
-amount = 6249
+coins = [1, 2,5]
+amount = 7
 print mytest.coinChange(coins, amount)
