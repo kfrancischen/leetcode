a0518c5c79c38ac492dd03e40b0ad34bc3f46632
francischen <kfrancischen@gmail.com>
2016-07-14 13:42:38 -0700

    finished 199

---
+++ python/199_binary-tree-right-side-view/binaryTreeRightSideView.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rightSideView(self, root):
+        def collect(node, depth):
+            if node:
+                if depth == len(view):
+                    view.append(node.val)
+                collect(node.right, depth+1)
+                collect(node.left, depth+1)
+        view = []
+        collect(root, 0)
+        return view
