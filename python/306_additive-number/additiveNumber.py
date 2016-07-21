0e7685448fcb1200bbc0c8aa8ad696c9bfb76271
francischen <kfrancischen@gmail.com>
2016-07-20 17:53:37 -0700

    finished 306

---
+++ python/306_additive-number/additiveNumber.py
@@ -0,0 +1,27 @@
+import itertools
+class Solution(object):
+    def isAdditiveNumber(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+        n = len(num)
+        for i, j in itertools.combinations(range(1, n), 2):
+            a, b = num[:i], num[i:j]
+            if a != str(int(a)) or b != str(int(b)):
+                continue
+            while j < n:
+                c = str(int(a) + int(b))
+                if not num.startswith(c, j):
+                    break
+                j += len(c)
+                a, b = b, c
+            if j == n:
+                return True
+
+        return False
+
+
+mytest = Solution()
+num = "0235813"
+print mytest.isAdditiveNumber(num)
