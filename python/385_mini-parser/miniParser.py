72e4276bf385d55c925bdd103bc708df0e458f7b
francischen <kfrancischen@gmail.com>
2016-08-15 14:07:09 -0700

    finished 385

---
+++ python/385_mini-parser/miniParser.py
@@ -0,0 +1,64 @@
+import ast
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested la nested list of integers represented as a strin
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution(object):
+    def deserialize(self, s):
+        """
+        :type s: str
+        :rtype: NestedInteger
+        """
+        result = NestedInteger()
+        s = ast.literal_eval(s)
+        if isinstance(s, int):
+            result.setInteger(s)
+        else:
+            self.decompose(result, s)
+        return result
+
+
+    def decompose(self, result, s):
+        for item in s:
+            newNestedInteger = NestedInteger()
+            if isinstance(item, int):
+                newNestedInteger.setInteger(item)
+            else:
+                element = self.decompose(newNestedInteger, item)
+            result.add(newNestedInteger)
+        return result
