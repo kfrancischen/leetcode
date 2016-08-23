b8672a08c5b2473684224793e87820fd7adb6515
francischen <kfrancischen@gmail.com>
2016-08-22 20:06:00 -0700

    finished 386, marked

---
+++ python/386_lexicographical-numbers/lexicographicalNumbers.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def lexicalOrder(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        highDigit = 1
+
+        while highDigit * 10 <= n:
+            highDigit *= 10
+        higherDigit = highDigit * 10
+        withKeys = []
+        for i in xrange(1, n+1):
+            key = i
+            while key < highDigit:
+                key *= 10
+            withKeys.append(key * higherDigit + i)
+            
+        withKeys.sort()
+        return [ki % higherDigit for ki in withKeys]
+
+mytest = Solution()
+n = 13
+print mytest.lexicalOrder(n)
