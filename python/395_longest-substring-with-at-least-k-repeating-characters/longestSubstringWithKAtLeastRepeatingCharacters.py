5cf9e721f0900734741d99c86e7bbfe93115c807
francischen <kfrancischen@gmail.com>
2016-09-06 15:08:11 -0700

    finished 395, marked

---
+++ python/395_longest-substring-with-at-least-k-repeating-characters/longestSubstringWithKAtLeastRepeatingCharacters.py
@@ -0,0 +1,19 @@
+from collections import Counter
+class Solution(object):
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        if len(s) < k:
+            return 0
+        c = min(set(s), key=s.count)
+        if s.count(c) >= k:
+            return len(s)
+        return max(self.longestSubstring(t, k) for t in s.split(c))
+
+mytest = Solution()
+s = "aaabb"
+k = 3
+print mytest.longestSubstring(s, k)
