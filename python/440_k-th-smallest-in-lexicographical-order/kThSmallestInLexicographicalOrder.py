c339c2c57cd863432a4cf37486a048ac39beb77a
francischen <kfrancischen@gmail.com>
2017-07-20 22:32:03 -0700

    commit again

---
+++ python/440_k-th-smallest-in-lexicographical-order/kThSmallestInLexicographicalOrder.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findKthNumber(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
---
+++ python/446_arithmetic-sliced-II-subsequence/arithmeticSlicedIISubsequence.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        
