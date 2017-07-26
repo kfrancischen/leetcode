f95c187f834d971c6ff15487f32cf51776aa713a
francischen <kfrancischen@gmail.com>
2017-07-25 23:34:12 -0700

    finished 621 and 636. 621 marked

---
+++ python/636_exclusive-time-of-functions/exclusiveTimeOfFunctions.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def exclusiveTime(self, n, logs):
+        """
+        :type n: int
+        :type logs: List[str]
+        :rtype: List[int]
+        """
+        ans = [0] * n
+        stack = []
+        prev_time = 0
+
+        for log in logs:
+            fn, typ, time = log.split(':')
+            fn, time = int(fn), int(time)
+
+            if typ == 'start':
+                if stack:
+                    ans[stack[-1]] += time - prev_time 
+                stack.append(fn)
+                prev_time = time
+            else:
+                ans[stack.pop()] += time - prev_time + 1
+                prev_time = time + 1
+
+        return ans
