192e2066f1f564ee88da0e8dac456b1ff04c86e7
Francis Chen <kaifengchen@pinterest.com>
2019-02-05 11:52:17 -0800

    finished 5

---
+++ 2019_round/5_longest_palindromic_substring/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if s == s[::-1]:
+            return s
+        maxLen=1
+        start=0
+        for i in xrange(1, len(s)):
+            if i-maxLen >=1 and s[i-maxLen-1:i+1]==s[i-maxLen-1:i+1][::-1]:
+                start=i-maxLen-1
+                maxLen+=2
+                continue
+
+            if i-maxLen >=0 and s[i-maxLen:i+1]==s[i-maxLen:i+1][::-1]:
+                start=i-maxLen
+                maxLen+=1
+        return s[start:start+maxLen]
