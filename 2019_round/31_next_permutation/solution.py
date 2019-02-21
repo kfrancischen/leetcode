a58374683e2bcec0e2beaa5b9a2bac4a264ec469
Francis Chen <kaifengchen@pinterest.com>
2019-02-20 16:59:10 -0800

    finished 31

---
+++ 2019_round/31_next_permutation/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 1:
+            return
+
+        left = len(nums) - 2
+        while left >= 0:
+            if nums[left] >= nums[left + 1]:
+                left -= 1
+            else:
+                for pivot in range(left+1, len(nums)):
+                    if nums[pivot - 1] > nums[left] and nums[pivot] <= nums[left]:
+                        pivot -= 1
+                        break
+
+                nums[pivot], nums[left] = nums[left], nums[pivot]
+                nums[left + 1:] = list(reversed(nums[left + 1:]))
+                return
+
+        if left == -1:
+            nums.reverse()
