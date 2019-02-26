8d5378cf4dc2feac85c710e930124f78bba958bf
Francis Chen <kaifengchen@pinterest.com>
2019-02-26 12:54:05 -0800

    finished 89

---
+++ 2019_round/89_gray_code/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        result = [0]
+        for i in range(0, n):
+            newArray = [val + 2 ** i for val in result]
+            result.extend(reversed(newArray))
+        return result
