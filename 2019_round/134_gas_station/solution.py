8752cd11e4670d92b267b4c3c631eca6465335f3
Francis Chen <kfrancischen@gmail.com>
2019-03-04 22:15:08 -0800

    finished 134

---
+++ 2019_round/134_gas_station/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        maxIdx = 0
+        if not gas or not cost:
+            return -1
+
+        total = 0
+        sumRemain = 0
+
+        diff = 0
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+            sumRemain += diff
+            if sumRemain < 0:
+                maxIdx = i + 1
+                sumRemain = 0
+            total += diff
+
+        return -1 if total < 0 else maxIdx
