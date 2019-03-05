dbcdb705ca99a2070c8cf83cbf1ca19e7544ed77
Francis Chen <kfrancischen@gmail.com>
2019-03-04 22:58:29 -0800

    finished 142

---
+++ 2019_round/142_linked_list_cycle_II/solution.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return None
+
+        pointer_1 = head
+        pointer_3 = head
+        pointer_2 = head
+
+        while True:
+            if pointer_2.next == None or pointer_2.next.next == None:
+                return None
+            pointer_2 = pointer_2.next.next
+            pointer_1 = pointer_1.next
+            if pointer_2 == pointer_1:
+                break
+
+        if pointer_1.next == head:
+            return head
+
+        while True:
+            if pointer_3 == pointer_1:
+                return pointer_3
+            pointer_3 = pointer_3.next
+            pointer_1 = pointer_1.next
