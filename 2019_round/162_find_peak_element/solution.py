28170c7c05987f8be4b20fbf7de66e7334b21408
Francis Chen <kfrancischen@gmail.com>
2019-03-07 23:10:26 -0800

    finished 162

---
+++ 2019_round/162_find_peak_element/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return 0
+        left = 0
+        right = len(nums) - 1
+        while left + 1 < right:
+            mid = (left + right) / 2
+
+            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+                return mid
+            if nums[mid] < nums[mid-1] and nums[mid] < nums[mid + 1]:
+                left = mid
+            elif nums[mid-1] < nums[mid] < nums[mid+1]:
+                left = mid
+            elif nums[mid-1] > nums[mid] > nums[mid+1]:
+                right = mid
+
+        if nums[left] > nums[right]:
+            return left
+        else:
+            return right
