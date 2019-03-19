caf361a90af9605c34f87b9234f87b54d8936157
Francis Chen <kfrancischen@gmail.com>
2019-03-18 21:43:23 -0700

    finished 221

---
+++ 2019_round/221_maximal_square/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+
+        side_len = [map(int, row) for row in matrix]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == '1':
+                    side_len[i][j] = 1 + min(side_len[i-1][j], side_len[i][j-1], side_len[i-1][j-1])
+
+        print side_len
+        return max(max(row) for row in side_len) ** 2
+
+test = Solution()
+matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+print test.maximalSquare(matrix)
