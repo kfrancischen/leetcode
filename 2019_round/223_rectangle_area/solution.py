90eed792fa3d3fe43a0e1826196b1998669a891c
Francis Chen <kfrancischen@gmail.com>
2019-03-18 22:08:20 -0700

    finished 223

---
+++ 2019_round/223_rectangle_area/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """
+        left_corner = [max(A, E), max(B, F)]
+        right_corner = [min(C, G)， min(D, H)]
+        cross = 0
+        if left_corner[0] < right_corner[0] and left_corner[1] < right_corner[1]:
+            cross = (right_corner[0] - left_corner[0]) * (right_corner[1] - left_corner[1])
+        return (C - A) * (D - B) + (G - E) * (H - F) - cross
